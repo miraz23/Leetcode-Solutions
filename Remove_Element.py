09e668d0164bd89b6ec7a09ca4c7592fbda2dd9f
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-02-26 16:12:25 +0600

    Day-25.1E

---
+++ Remove_Element.py
@@ -0,0 +1,10 @@
+# Link: https://leetcode.com/problems/remove-element/
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        j = 0    
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
