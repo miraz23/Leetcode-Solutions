f340b3683e608e0f148769d5997b2b98e9b6c362
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-02-28 23:56:32 +0600

    Day-27.1E

---
+++ Merge_Sorted_Array.py
@@ -0,0 +1,14 @@
+# Link: https://leetcode.com/problems/merge-sorted-array/
+
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        p1, p2, i = m - 1, n - 1, m + n - 1
+        
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[i] = nums2[p2]
+                p2 -= 1
+            i -= 1
