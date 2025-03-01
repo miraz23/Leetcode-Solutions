31d853aba57435f095b571003801f563449d0255
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-01 19:55:13 +0600

    Day-28.1E

---
+++ Apply_Operations_to_an_Array.py
@@ -0,0 +1,12 @@
+# Link: https://leetcode.com/problems/apply-operations-to-an-array/
+
+class Solution(object):
+    def applyOperations(self, nums):
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+
+        non_zero = [num for num in nums if num != 0]
+        zeros = [0] * (len(nums) - len(non_zero))
+        return non_zero + zeros
