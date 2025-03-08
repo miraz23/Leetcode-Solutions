7ec262bcd34b2480fa53324b59ade56ec7570033
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-08 22:58:18 +0600

    Day-34.2M

---
+++ Remove_Duplicates_from_Sorted_Array_II.py
@@ -0,0 +1,15 @@
+# Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if len(nums) <= 2:
+            return len(nums)
+
+        j = 2
+        
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
