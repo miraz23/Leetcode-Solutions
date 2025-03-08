c6864ebee53f478341cc47ff0a3fc014ae102052
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-08 22:56:16 +0600

    Day-34.1M

---
+++ Rotate_Image.py
@@ -0,0 +1,11 @@
+# Link: https://leetcode.com/problems/rotate-image/
+
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            matrix[i].reverse()     
