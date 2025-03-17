5d32a2525dab69158bec4d27e1764ecf4de8dabf
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-17 23:09:38 +0600

    Day-43.2E

---
+++ Modify_Columns.py
@@ -0,0 +1,7 @@
+# Link: https://leetcode.com/problems/modify-columns/
+
+import pandas as pd
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["salary"] = employees["salary"] * 2
+    return employees
