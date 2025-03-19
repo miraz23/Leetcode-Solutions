42f2113c20c2394c4e45ca8e0b3ec26c933f7f69
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-19 23:04:30 +0600

    Day-45.2E

---
+++ Build_Array_from_Permutation.py
@@ -0,0 +1,5 @@
+# Link: https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution(object):
+    def buildArray(self, nums):
+        return [nums[nums[i]] for i in range(len(nums))]
