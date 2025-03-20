c08b0975d30f9ab18a788c58031f12822f7cdce4
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-20 16:50:38 +0600

    Day-46.3M

---
+++ Integer_to_Roman.py
@@ -0,0 +1,28 @@
+# Link: https://leetcode.com/problems/integer-to-roman/
+
+class Solution:
+    def intToRoman(self, num):
+        roman = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+        
+        result = ""
+        
+        for value, symbol in roman:
+            while num >= value:
+                num -= value
+                result += symbol
+        
+        return result
