e61e1dc4bf38fd0c06694b0915df5f40a7ff57e7
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-04-07 23:55:42 +0600

    Day-65.1E

---
+++ Smallest Even Multiple.py
@@ -0,0 +1,8 @@
+# Link: https://leetcode.com/problems/smallest-even-multiple/
+
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        if n % 2 == 0:
+            return n
+        else:
+            return n * 2
